461d31bfe37fcf818bb690fb88816f24a0955cde
Scott Cranton <scranton@fusesource.com>
2011-05-19 05:59:00 -0700

    initial commit

---
+++ src/main/java/com/fusesource/examples/activemq/SimpleConsumer.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2011 FuseSource
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.fusesource.examples.activemq;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class SimpleConsumer {
+    private static final Log LOG = LogFactory.getLog(SimpleConsumer.class);
+
+    private static final Boolean NON_TRANSACTED = false;
+    private static final String CONNECTION_FACTORY_NAME = "myJmsFactory";
+    private static final String DESTINATION_NAME = "queue/simple";
+    private static final int MESSAGE_TIMEOUT_MILLISECONDS = 120000;
+
+    public static void main(String args[]) {
+        Connection connection = null;
+
+        try {
+            // JNDI lookup of JMS Connection Factory and JMS Destination
+            Context context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
+            Destination destination = (Destination) context.lookup(DESTINATION_NAME);
+
+            connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            LOG.info("Start consuming messages from " + destination.toString() + " with " + MESSAGE_TIMEOUT_MILLISECONDS + "ms timeout");
+
+            // Synchronous message consumer
+            int i = 1;
+            while (true) {
+                Message message = consumer.receive(MESSAGE_TIMEOUT_MILLISECONDS);
+                if (message != null) {
+                    if (message instanceof TextMessage) {
+                        String text = ((TextMessage) message).getText();
+                        LOG.info("Got " + (i++) + ". message: " + text);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            consumer.close();
+            session.close();
+        } catch (Throwable t) {
+            LOG.error(t);
+        } finally {
+            // Cleanup code
+            // In general, you should always close producers, consumers,
+            // sessions, and connections in reverse order of creation.
+            // For this simple example, a JMS connection.close will
+            // clean up all other resources.
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    LOG.error(e);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/fusesource/examples/activemq/SimpleProducer.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2011 FuseSource
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.fusesource.examples.activemq;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class SimpleProducer {
+    private static final Log LOG = LogFactory.getLog(SimpleProducer.class);
+
+    private static final Boolean NON_TRANSACTED = false;
+    private static final long MESSAGE_TIME_TO_LIVE_MILLISECONDS = 0;
+    private static final int MESSAGE_DELAY_MILLISECONDS = 100;
+    private static final int NUM_MESSAGES_TO_BE_SENT = 100;
+    private static final String CONNECTION_FACTORY_NAME = "myJmsFactory";
+    private static final String DESTINATION_NAME = "queue/simple";
+
+    public static void main(String args[]) {
+        Connection connection = null;
+
+        try {
+            // JNDI lookup of JMS Connection Factory and JMS Destination
+            Context context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
+            Destination destination = (Destination) context.lookup(DESTINATION_NAME);
+
+            connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(destination);
+
+            producer.setTimeToLive(MESSAGE_TIME_TO_LIVE_MILLISECONDS);
+
+            for (int i = 1; i <= NUM_MESSAGES_TO_BE_SENT; i++) {
+                TextMessage message = session.createTextMessage(i + ". message sent");
+                LOG.info("Sending to destination: " + destination.toString() + " this text: '" + message.getText());
+                producer.send(message);
+                Thread.sleep(MESSAGE_DELAY_MILLISECONDS);
+            }
+
+            // Cleanup
+            producer.close();
+            session.close();
+        } catch (Throwable t) {
+            LOG.error(t);
+        } finally {
+            // Cleanup code
+            // In general, you should always close producers, consumers,
+            // sessions, and connections in reverse order of creation.
+            // For this simple example, a JMS connection.close will
+            // clean up all other resources.
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    LOG.error(e);
+                }
+            }
+        }
+    }
+}
